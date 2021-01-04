ab15d1ce6350483aa890f403d88154033251c226
Ashen <shenalexw@gmail.com>
2021-01-03 17:04:06 -0800

    Solve function created, but will not append changes to board.

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -10,52 +12,65 @@ board = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
-"""
+
+def clear(game):
+    for row in range(len(game)):
+        for column in range(len(game)):
+            game[row][column] = 0
+
+
 def solve(game):
-"""
+    for row in range(len(game)):
+        for column in range(len(game)):
+            if game[row][column] == 0:
+                for num in range(1, 10):
+                    if check(game, (row, column), num):
+                        game[row][column] = num
+                        if solve(game):
+                            return True
+                        game[row][column] = 0
+                return False
+
 
 
 def print_board(game):
-    for row in range(len(board)):
+    for row in range(len(game)):
         if row % 3 == 0 and row != 0:
             print("=================================")
-        for column in range(len(board)):
+        for column in range(len(game)):
             if column % 3 == 0:
                 print(" || ", end=" ")
             if column == 8:
-                print(board[row][column], end="\n")
+                print(game[row][column], end="\n")
             else:
-                print(str(board[row][column]) + " ", end="")
+                print(str(game[row][column]) + " ", end="")
 
 
-def num_input(location, number):
+def num_input(game, location, number):
     row = location[0]
     column = location[1]
-    if check(location, number):
-        board[row][column] = number
+    if check(game, location, number):
+        game[row][column] = number
     else:
         return "wrong move"
 
 
-def check(location, number):
+def check(game, location, number):
     row = location[0]
     column = location[1]
     row_box = (location[0] // 3) * 3
     column_box = location[1] // 3 * 3
-    if board[row][column] != 0:
+    if game[row][column] != 0:
         return False
-    for row_value in range(len(board)):
-        if board[row][row_value] == number:
+    for row_value in range(len(game)):
+        if game[row][row_value] == number:
             return False
-    for col_value in range(len(board)):
-        if board[col_value][column] == number:
+    for col_value in range(len(game)):
+        if game[col_value][column] == number:
             return False
     for row_value in range(row_box, row_box + 3):
         for col_value in range(column_box, column_box + 3):
-            if board[row_value][col_value] == number:
+            if game[row_value][col_value] == number:
                 return False
     return True
-
-print(num_input((1,1), 1))
-print_board(board)
 
